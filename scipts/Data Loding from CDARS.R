b3132fc49bd6b79f6b1cc727650c03f174086ab6
fm <cainefm@gmail.com>
2020-06-26 17:23:13 +0800

    Add trycatch and supress the name warning.
    Add tryCatch in the function, change the make names outside of the readxl in order to suppress the warnings

--- scipts/Data Loding from CDARS.R
+++ scipts/Data Loding from CDARS.R
@@ -13,36 +13,44 @@
 # Sys.Date()  History
 # ----------------------------------------------------------------------------------------------------------------------------
 # 20200623    The old code will be used and new functions,checking the consistancy of the raw data, will tried to add into that.
-
-
-# ----------------------------------------------------------------------------------------------------------------------------
-# Mannul function ----
-# ----------------------------------------------------------------------------------------------------------------------------
-
+# 20200626    Trying to appy trycatch, but i can't pass the argument to that so i dont know how to warn with the error file.
 
 # ----------------------------------------------------------------------------------------------------------------------------
 #   validate the name of columns 
 # ----------------------------------------------------------------------------------------------------------------------------
-my_custom_name_repair <- function(x) tolower(gsub("\\.{1,}",'\\.',gsub("\n|  ", "", make.names(x))))
+# my_custom_name_repair <- function(x) tolower(gsub("\\.{1,}",'\\.',gsub("\n|  ", "", make.names(x))))
+# Change into the function in janitor
 
 
-
+# ----------------------------------------------------------------------------------------------------------------------------
+#   Check consistancy of the datasets
+# ----------------------------------------------------------------------------------------------------------------------------
 check_consistancy_columns <- function(directory){
-    list.df<- lapply(list.files(path=directory,full.names = T),readxl::read_xlsx,.name_repair=my_custom_name_repair,n_max=1)
-    return(lapply(list.df,colnames))
+    list.df<- lapply(list.files(path=directory,full.names = T),readxl::read_xlsx,n_max=1)
+    list.colnames <- lapply(list.df,colnames)
+    if(length(unique(list.colnames))==1){
+        return('All data have the same columns')
+    }else{
+        return(as.data.frame(table(unique(lapply(list.colnames,paste0,collapse=',')))) %>% rename(colnames=Var1))
+    }
 }
 
 
-merge_files <- function(files,numberofrows=Inf){
-    list_df <- lapply(files,readxl::read_xlsx,.name_repair=my_custom_name_repair,n_max=numberofrows) 
+merge_files <- function(directory){
+    tryCatch({
+        check_consistancy_columns(directory)
+        },
+        error = function(err){
+            message(paste("URL does not seem to exist:", url))
+            message("Here's the original error message:")
+            message(cond)
+            return(NULL)
+        }
+    )
+    files <- list.files(path=directory,full.names = T)
+    list_df <-   pbapply::pblapply(files,readxl::read_xlsx)
     df <- list_df %>% bind_rows(.)
-    print(files)
-    return(df)
-}
-
-
-merge_files_xls <- function(files){
-    list_df <- lapply(files,readxl::read_xls,.name_repair=my_custom_name_repair) 
-    df <- list_df %>% bind_rows(.)
+    colnames(df) <- janitor::make_clean_names(colnames(df))
+    # print(files)
     return(df)
 }
